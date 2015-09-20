2956fea7df80f34bf588ad75cad31fd0a4091f32
aska <aska.lee@gmail.com>
2015-09-20 23:26:30 +0800

    ready

--- MergeDataAndDoc/Program.cs
+++ MergeDataAndDoc/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace MergeDataAndDoc
     {
         static void Main(string[] args)
         {
+            string inputFileName = "defaultInput.txt";
+            string outputFileName = "defaultOutput.txt";
+            if (args.Length == 2)
+            {
+                inputFileName = args[0];
+                outputFileName = args[1];
+            }
+
+            using (StreamReader inputFile = new StreamReader(inputFileName))
+            using(StreamWriter outputFile = new StreamWriter(outputFileName))
+            {
+                string line;
+                while((line = inputFile.ReadLine()) != null)
+                {
+                    string outputLine = "***" + line;
+                    Console.WriteLine("Write line: " + outputLine);
+                    outputFile.WriteLine(outputLine);
+                }
+            }
         }
     }
 }
